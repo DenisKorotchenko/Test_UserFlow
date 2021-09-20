18c15d3e7a298353a6bc1b9efe6ce2cd2f0c4f0e
DenisKorotchenko <dzd1024ozd@gmail.com>
2021-09-20 11:18:03 +0300

    Initial commit

---
+++ src/main/kotlin/Crawler.kt
@@ -0,0 +1,93 @@
+import org.jsoup.Jsoup
+import java.io.File
+import java.util.concurrent.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+
+class Crawler(private val protocol: String, private val domain: String, private val onlyMainDomain: Boolean) {
+
+    private val lists = ConcurrentHashMap<String, List<String>>()
+
+    private val queue = Channel<String>(Channel.Factory.UNLIMITED)
+    private val used = ConcurrentHashMap<String, Boolean>()
+
+    private suspend fun tryAdd(inputUrl: String) {
+        val url = fixUrl(inputUrl.split("#")[0])
+        if (((!onlyMainDomain && url.contains(Regex("^$protocol://[^/]*$domain")))
+                || (onlyMainDomain && url.contains(Regex("^$protocol://$domain"))))
+            && !url.contains("#") && used[url] != true
+        )
+        {
+            used[url] = true
+            queue.send(url)
+        }
+    }
+
+    private fun fixUrl(url: String): String {
+        return url
+            .replace(Regex("^.*://"), "$protocol://")
+            .replace(Regex("[^:/]/+")) { p -> p.value[0] + "/" }
+            .replace(Regex("^$protocol://www[.]"), "$protocol://")
+    }
+
+    suspend fun run(numPermits: Int, scope: CoroutineScope) {
+        var i = 0
+        used.clear()
+        lists.clear()
+
+        val startUrl = fixUrl("$protocol://$domain")
+        used[startUrl] = true
+        queue.send(startUrl)
+
+        val list = mutableListOf<String>()
+
+        val permits = Semaphore(numPermits)
+
+        while (!queue.isEmpty || permits.availablePermits() < numPermits) {
+            val maybeUrl = queue.tryReceive()
+            val currentUrl = maybeUrl.getOrNull()
+            if (!maybeUrl.isSuccess || currentUrl == null) {
+                continue
+            }
+            permits.acquire()
+            GlobalScope.launch {
+                try {
+                    println("$i $currentUrl")
+                    i++
+                    val html = Jsoup.connect(currentUrl).ignoreHttpErrors(true).get()
+                    for (a in html.select("a")) {
+                        val absRef = fixUrl(a.absUrl("href"))
+                        list.add(absRef)
+                        tryAdd(absRef)
+                    }
+
+                    lists[currentUrl] = list
+                } catch (e: Exception) {
+                    println(e)
+                } finally {
+                        permits.release()
+                }
+            }
+        }
+    }
+
+    fun writeToFile(fileName: String) {
+        File(fileName).printWriter().use { out ->
+            for (list in lists) {
+                out.print(list.key + " :")
+                for (ref in list.value) {
+                    out.print(" $ref")
+                }
+                out.println()
+            }
+        }
+
+    }
+}
+
+fun main() {
+    val crawler = Crawler("https", "jetbrains.com", true)
+    runBlocking { crawler.run(256, this) }
+    //println("Start writing to file")
+    //crawler.writeToFile("output.txt")
+}
